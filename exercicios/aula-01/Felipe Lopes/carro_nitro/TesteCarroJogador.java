a9386889e7f3f1a7a874618382f4b187dae13be2
felipeee98 <felipeee987172@gmail.com>
2026-03-28 12:38:01 -0300

    Exercicio 1 - Teste Carro Nitro

---
+++ exercicios/aula-01/Felipe Lopes/carro_nitro/TesteCarroJogador.java
@@ -0,0 +1,22 @@
+package estudos;
+
+public class TesteCarroJogador {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+        CarroJogador carro = new CarroJogador();
+
+        carro.marca = "honda";
+        carro.modelo = "civic";
+        carro.ano = 2009;
+        carro.nivelNitro = 20;
+
+        carro.usarNitro();
+        carro.usarNitro();
+        carro.usarNitro();
+    }
+
+	}
+
+
