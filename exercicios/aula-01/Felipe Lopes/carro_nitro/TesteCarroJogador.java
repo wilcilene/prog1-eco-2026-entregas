492da976467661a182d674a6c0d06e6fb8ceec26
felipeee98 <felipeee987172@gmail.com>
2026-03-28 12:33:55 -0300

    Implement main method for CarroJogador testing
    Added a main method to test the CarroJogador class functionality, including setting properties and using nitro.

--- exercicios/aula-01/Felipe Lopes/carro_nitro/TesteCarroJogador.java
+++ exercicios/aula-01/Felipe Lopes/carro_nitro/TesteCarroJogador.java
@@ -1,1 +1,23 @@
+package estudos;
+
+public class TesteCarroJogador {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+        CarroJogador carro = new CarroJogador();
+
+        carro.marca = "honda";
+        carro.modelo = "civic";
+        carro.ano = 2009;
+        carro.nivelNitro = 20;
+
+        carro.usarNitro();
+        carro.usarNitro();
+        carro.usarNitro();
+    }
+
+	}
+
+
 
