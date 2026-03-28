c53b6a18e24811645494fdc6f27bf7f1b809c07c
felipeee98 <felipeee987172@gmail.com>
2026-03-28 13:15:44 -0300

    Add files via upload

---
+++ exercicios/aula-01/Felipe Lopes/estudante/TesteEstudante.java
@@ -0,0 +1,21 @@
+package estudante;
+
+public class TesteEstudante {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Estudante estudante1 = new Estudante();
+		
+		estudante1.nome = "Felipe lopes";
+		estudante1.matricula ="23343";
+		
+		System.out.println("nome estudante :" + estudante1.nome);
+		System.out.println("matricula:" + estudante1.matricula);
+		
+		
+		
+		
+	}
+
+}
