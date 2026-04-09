8bcaa4257abe806ea51d8b4ecb36798ba4e54677
felipeee98 <felipeee987172@gmail.com>
2026-04-09 19:02:57 -0300

    Add files via upload

---
+++ exercicios/aula-05/Felipe Lopes/Pontuação/dasdad.java
@@ -0,0 +1,19 @@
+package exercicios1;
+public class main{
+
+public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	Carrogamer carro = new Carrogamer();
+	carro.modelo = "super dev gt";
+	carro.velo = 120; 
+	carro.quantidadeNitro= 60;
+	
+	System.out.println("Modelo:");
+	System.out.print("\n");
+	
+	
+	
+	
+	
+	}
+}
