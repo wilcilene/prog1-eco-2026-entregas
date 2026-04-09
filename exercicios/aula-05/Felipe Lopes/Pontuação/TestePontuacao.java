d8b2ac11e1543ec3c521447b2228bf910c9bf2fc
felipeee98 <felipeee987172@gmail.com>
2026-04-09 19:03:28 -0300

    Add files via upload

---
+++ exercicios/aula-05/Felipe Lopes/Pontuação/TestePontuacao.java
@@ -0,0 +1,37 @@
+package Pontuação;
+
+class Jogador {
+    String nome;
+    int pontuacao;
+}
+
+public class TestePontuacao {
+
+    public static void main(String[] args) {
+
+        Jogador[] jogadores = new Jogador[3];
+
+        jogadores[0] = new Jogador();
+        jogadores[0].nome = "Victor";
+        jogadores[0].pontuacao = 120;
+
+        jogadores[1] = new Jogador();
+        jogadores[1].nome = "Eduardo";
+        jogadores[1].pontuacao = 200;
+
+        jogadores[2] = new Jogador();
+        jogadores[2].nome = "Felipe";
+        jogadores[2].pontuacao = 180;
+
+        Jogador campeao = jogadores[0];
+
+        for (int i = 1; i < jogadores.length; i++) {
+            if (jogadores[i].pontuacao > campeao.pontuacao) {
+                campeao = jogadores[i];
+            }
+        }
+
+        System.out.println("Campeão: " + campeao.nome
+                + " com " + campeao.pontuacao + " pontos.");
+    }
+}
