438303f6e33a1b46b042b9df26059ef2d46bc344
felipeee98 <felipeee987172@gmail.com>
2026-04-10 18:23:30 -0300

    Add files via upload

---
+++ exercicios/aula-07/Felipe Lopes/MediaTurma.java
@@ -0,0 +1,43 @@
+package MediaTurma;
+import java.util.Scanner;
+
+public class MediaTurma {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("quantos alunos ");
+        int n = entrada.nextInt();
+
+        double[] notas = new double[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("nota aluno " + (i + 1) + " : ");
+            notas[i] = entrada.nextDouble();
+        }
+
+        double soma = 0.0;
+        int abaixo6 = 0, entre6e79 = 0, acima8 = 0;
+
+        for (int i = 0; i < n; i++) {
+            soma += notas[i];
+
+            if (notas[i] < 6) {
+                abaixo6++;
+            } else if (notas[i] < 8) {
+                entre6e79++;
+            } else {
+                acima8++;
+            }
+        }
+
+        double media = soma / n;
+
+        System.out.println("Média: " + media);
+        System.out.println("Abaixo de 6: " + abaixo6);
+        System.out.println("Entre 6 e 7.9: " + entre6e79);
+        System.out.println("8 ou mais: " + acima8);
+
+        entrada.close();
+    }
+}
